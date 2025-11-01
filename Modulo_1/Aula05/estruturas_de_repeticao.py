f72576ae81fda9cd541cb33bafce0e9282d8c73d
THIAGO FELIX LEAL PINTO <felix.thiago@aluno.senac.rio>
2025-10-31 21:52:16 -0300

    Atualização 1 -  31.10.2025

--- Modulo_1/Aula05/estruturas_de_repeticao.py
+++ Modulo_1/Aula05/estruturas_de_repeticao.py
@@ -1,8 +1,8 @@
-# #Usando for (Quando sabemos a contagem: 5 vezes nesse caso)
+#Usando for (Quando sabemos a contagem: 5 vezes nesse caso)
 
-# for i in range(5):
+# for i in range(5):# range começa do zero(0), range será(0, 1, 2, 3, 4,)
 #     try:
-#         print(f'Número{i+1} de 5:')
+#         print(f'Número{i+1} de 5:')# é pq o range começa do zero.
 #         num= float(input('Digite um número: '))
         
 #         dobro= num*2
@@ -19,7 +19,7 @@
 # contador= 0# Inicializamos o contador.
 # limite= 5# Definimos o limite.
 
-# while contador<limite: #A condição de parada: Enquanto o contador for menor que 5.
+# while contador < limite: #A condição de parada: Enquanto o contador for menor que 5.
 #     try:
 #         print(f"{contador+1} de {limite}:")
 #         num= float(input('Digite um número: '))
@@ -29,12 +29,12 @@
 #         quadruplo= num*4
 #         print(f"Resultado: Dobro={dobro}, Triplo={triplo}, Quádruplo={quadruplo}\n")
 
-#         contador= contador+1# Importantíssimo! Incrementa o contador para evitar loop infinito.
+#         contador= contador+1# Importantíssimo! Incrementa o contador para evitar loop infinito. Informa que quer um novo ciclo.
 
 #     except ValueError:
 #         print("Entrada inválida. Tente novamente.")
 
-# Usando do-while (Executar primeiro, checar depois)
+# #Usando do-while (Executar primeiro, checar depois)
 # print("--- Simulação DO-WHILE (Executa 1° vez, depois checa) ---")
 
 # contador= 0
